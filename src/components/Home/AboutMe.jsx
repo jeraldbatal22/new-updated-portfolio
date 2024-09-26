1de6810c4150021609fa5206014f233896450c2d
Jerald <jebatal@highlysucceed.com>
2024-09-26 10:43:05 +0800

    Rephrase about me

--- src/components/Home/AboutMe.jsx
+++ src/components/Home/AboutMe.jsx
@@ -34,11 +34,11 @@ const AboutMe = () => {
             <div className="">
               <h1 className="text-[30px] text-blue-500">Introduction</h1>
               {/* <p>Hello, my name is Jerald. You can call me JB, Jer, or Rald. I am 4 years graduated in Bachelor of Science degree in Hotel and Restaurant Management. I recently made a career shift into the field of software engineering to further develop my coding skills and knowledge. I discovered a passion for building and publishing websites along the way. My career goal is to become a highly skilled and professional web developer, with aspirations of becoming a full-stack developer in the future. In my free time, I enjoy playing basketball, mobile games, and computer games. I also find relaxation in playing the guitar instrumentally and arranging music. Additionally, I like to watch K-Dramas and Netflix movies when I feel stuck or bored with coding.</p> */}
-              <p>Hello, I’m Jerald, but you can call me JB, Jer, or Rald. I am 4 years graduated of Bachelor Science in Hotel and Restaurant Management. I made a significant career shift into software engineering to hone my coding skills and deepen my knowledge in the field. Along this journey, I discovered a strong passion for building and publishing websites.</p>
+              <p>Hello, I’m Jerald, but you can call me JB, Jer, or Rald. I graduated with a degree in Hotel and Restaurant Management, but I decided to switch careers and become a front-end developer in IT.</p>
               <br/>
               <p>My current career goal is to become a highly skilled and professional web developer, with the aspiration of eventually transitioning into a full-stack developer role. This involves mastering both the front-end and back-end aspects of web development to create comprehensive and robust applications.</p>
               <br/>
-              <p>In my leisure time, I indulge in playing basketball, mobile games, and computer games. I also find solace in playing the guitar, whether it’s strumming out instrumental pieces or arranging music. Watching K-Dramas and Netflix movies is another way I unwind, especially when I’m feeling stuck or need a break from coding.</p>
+              <p>In my free time, I enjoy playing basketball, mobile and computer games, and playing guitar its either singing or instrumental music. I also like watching K-Dramas and Netflix movies to relax when I need a break from coding.</p>
             </div>
           </div>
         </div>
